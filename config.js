9b0675b5ee4758d1a73fea3b0345d3ed4ad5bbd5
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-09-05 13:51:07 -0400

    Update Site

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ const config = {
   "header": {
     "tag": "CASE STUDY",
     "title": "How We Helped Toronto Downsizers Save $69,000 and Find Their Dream Condo After 8 Months of Searching",
-    "subheadline": "Purchased in a <strong>High-Demand Area</strong> With Strong Terms—Now <a href=\"#\">Generating Top-Dollar Rent</a> in a Tight Market"
+    "subheadline": "Purchased in a <strong>High-Demand Area</strong> With Strong Terms—Now Generating Top-Dollar Rent in a Tight Market"
   },
   "mainBox": {
     "summary": {
